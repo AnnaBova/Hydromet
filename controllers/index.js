e8f2833c3a5d445f77b795a0980040d94d245afb
AnnaBova <anna.bova23@gmail.com>
2018-06-04 11:43:34 +0300

    Data out of template

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,15 @@
-var WeatherDays = require('../data/WeatherObl.json');
-var waterTemperature = require('../data/waterTemperature.json');
-var zptemperature = require('../data/zptemperature.json');
-var CityWeatherTable = require('../data/CityWeatherTable.json');
-var weatherCity = require('../data/weatherCity.json');
-var textWeatherRegion = require('../data/TextWeatherRegion.json');
-var textWeatherCity = require('../data/TextWeatherCity.json');
+var WeatherDays = require('../data/WeatherObl');
+var waterTemperature = require('../data/waterTemperature');
+var zptemperature = require('../data/zptemperature');
+var CityWeatherTable = require('../data/CityWeatherTable');
+var weatherCity = require('../data/weatherCity');
+var textWeatherRegion = require('../data/TextWeatherRegion');
+var textWeatherCity = require('../data/TextWeatherCity');
+var ClimaticData = require('../data/climatic_date');
+var Regular_Observation = require('../data/regular_observations');
+var meterolog_phenomena = require('../data/meteorolog_phenomena');
+var events = require('../data/event');
+var climatic_records = require('../data/climatic_records');
 
 module.exports = {
   getMainPage: function (req, res) {
@@ -48,27 +53,28 @@ module.exports = {
     res.render('pages/structure');
   },
   getMeteorologPhenomena : function(req, res){
-    res.render('pages/meteorological_phenomena');
+    res.render('pages/meteorological_phenomena', {modalId: meterolog_phenomena.modalIndex});
   },
   getHydrometeorologyBulletin: function(req, res){
     res.render('pages/hedrometeorological_bulletin', 
     { weatherObl: WeatherDays.days,
       TextWeatherObl:textWeatherRegion.days, 
       weatherCity: weatherCity.days, 
-      TextWeatherCity: textWeatherCity.days 
+      TextWeatherCity: textWeatherCity.days,
+      ClimaticData: ClimaticData
     });
   },
   getClimaticCharacteristic: function(req, res){
     res.render('pages/climatic_characteristic');    
   },
   getClimaticRecords: function(req, res){
-    res.render('pages/climatic_records');
+    res.render('pages/climatic_records', { modalId: climatic_records.modalIndex });
   },
   getRegularObservations: function(req, res){
-    res.render('pages/regular_observations');
+    res.render('pages/regular_observations', { data:Regular_Observation });
   },
   getEvents: function(req, res){
-    res.render('pages/events');
+    res.render('pages/events', {events: events.events});
   },
   getSingleEvents: function(req, res){
     res.render('pages/single_events');
