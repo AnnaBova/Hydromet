9645cbe95887630554bd68fa1644eb50a991c3b7
AnnaBova <anna.bova23@gmail.com>
2018-08-03 14:18:32 +0300

    clean from old build files

--- adminka/after.js
+++ adminka/after.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var ncp = require('ncp').ncp;
+const fs = require('fs-extra');
 
 ncp.limit = 16;
 
 var srcPath = __dirname + '/build'; //current folder
 var destPath = '../express_ejs/public/build'; //Any destination folder
+
+fs.removeSync(destPath);
 
 console.log('Copying files...');
 ncp(srcPath, destPath, function (err) {
